67d5fe9fead41819837bc40b4ab7153ca3f60776
Valentine <kerubovalentine1@gmail.com>
2023-06-20 15:18:03 +0300

    complete assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -18,7 +18,8 @@ class BirdsController < ApplicationController
 
   #POST /birds
   def create
-    byebug
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
